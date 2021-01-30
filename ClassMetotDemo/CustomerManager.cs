88c8d1eef0a174d6b9b578b8a9bbac4211c85b85
Alihan Demirdaş <alihandemirdas@outlook.com>
2021-01-30 16:49:43 +0300

    customer service basic automation

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+
+            Console.WriteLine("Added Customer: \n\n" + customer.Id + "\n" + customer.Name + "\n" + customer.Surname + "\n" + customer.Email + "\n" +customer.PhoneNumber);
+
+        }
+
+        public void List(List<Customer> listedcustomers)
+        {
+
+            foreach(Customer customer in listedcustomers)
+            {
+
+                Console.WriteLine("Listed Customer: \n\n" + customer.Id + "\n" + customer.Name + "\n" + customer.Surname + "\n" + customer.Email + "\n" + customer.PhoneNumber);
+
+            }
+
+
+        }
+
+
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using ClassMethodDemo;
+using System;
+using System.Collections.Generic;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Alihan";
+            customer1.Surname = "Demirdas";
+            customer1.Email = "alihan@gmail.com";
+            customer1.PhoneNumber = 0141431414;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "İbrahim";
+            customer2.Surname = "Demirdas";
+            customer2.Email = "ibrahim@gmail.com";
+            customer2.PhoneNumber = 0141431415;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Ferhan";
+            customer3.Surname = "Demirdas";
+            customer3.Email = "ferhan@gmail.com";
+            customer3.PhoneNumber = 0141431416;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+
+            Console.WriteLine("\n---Registered Customers in System---\n");
+
+            List<Customer> listedCustomers = new List<Customer> { customer1, customer2, customer3 };
+            customerManager.List(listedCustomers);
+            
+
+           
+
+
+
+
+        }
+    }
+}
