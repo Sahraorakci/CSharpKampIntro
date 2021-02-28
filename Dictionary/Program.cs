e9816fb7ae0d742a36d3fa175801fbcc71cc085d
Alihan Demirdaş <45637245+alihandemirdas@users.noreply.github.com>
2021-02-28 18:37:38 +0300

    Add files via upload

---
+++ Dictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyDictionary<int, string> musteriler = new MyDictionary<int, string>();
+            musteriler.Add(1, "Alihan");
+            musteriler.Add(2, "Ferhan");
+            musteriler.Add(3, "İbrahim");
+
+            for (int i = 0; i < musteriler.Count(); i++)
+            {
+                Console.WriteLine(musteriler.keys[i] + " " + musteriler.values[i]);
+            }
+
+
+            Console.WriteLine("Number of customers :" + musteriler.Count());
+        }
+    }
+}
